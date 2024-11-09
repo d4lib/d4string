6100970a02a4f1d4d92c66836a22f04260808362
Danin <daninsadzak@gmail.com>
2024-11-09 23:04:42 +0100

    update example.c

--- example.c
+++ example.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "d4string.h"
 
 int main() {
-    printf("Hello, world!\n");
+    char *string = "I like";
+    printf("%s\n", d4concatenate(string, " chocolate"));
     return 0;
 }
